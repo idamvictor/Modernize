dfdfc69c3e0d4c4e1e5b376546053e130e2c69a1
victor <idamvictorx1@gmail.com>
2024-11-19 10:37:57 +0100

    next js config for cors

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api-demo-8xxq.onrender.com/api/:path*', // Proxy to Backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
